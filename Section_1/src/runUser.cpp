eb2e3a20298f23b89a7048942405641db5e72319
HerrJH <43826150+HerrJH@users.noreply.github.com>
2020-03-10 22:34:07 +0100

    H-1 update json files for compiling and debuging

--- Section_1/src/runUser.cpp
+++ Section_1/src/runUser.cpp
@@ -1,9 +1,9 @@
-#include "runUser.hpp"
-#include "iostream"
+#include "../inc/runUser.hpp"
+#include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-    
-    return 0;
+    std::cout << "Hello World! Hello Jian!" << std::endl;
+    system("pause");
+    return 0; //0 means no error, -1 means error.
 }
